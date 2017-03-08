2614156b3733938750c01c197623e46bacb988ba
Boaz Mohar <boazmohar@gmail.com>
2017-03-08 15:13:08 -0500

    pypi testing

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pySparkUtils',
+  packages = ['pySparkUtils'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A collection of utilities for handling pySpark's SparkContext',
+  author = 'Boaz Mohar',
+  author_email = 'boazmohar@gmail.com',
+  url = 'https://github.com/boazmohar/pySparkUtils', # use the URL to the github repo
+  download_url = 'https://github.com/boazmohar/pySparkUtils/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['spark', 'pyspark',],
+  classifiers = [],
+)
