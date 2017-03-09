188340f28ea78dbc5ba42bef55a165abab4d5e18
Boaz Mohar <boazmohar@gmail.com>
2017-03-09 15:25:46 -0500

    new tests to fallback function

--- test/test_fallback.py
+++ test/test_fallback.py
@@ -3,7 +3,7 @@ import pytest
 pytestmark = pytest.mark.usefixtures("eng")
 
 
-def test_fallback(eng):
+def test_fail(eng):
     target_cores = eng.defaultParallelism
     @fallback
     def test_func(sc_inner):
@@ -13,3 +13,34 @@ def test_fallback(eng):
     sc = test_func(eng)
     assert sc.defaultParallelism == target_cores
     sc.stop()
+
+
+def test_pass(eng):
+    target_cores = eng.defaultParallelism
+    @fallback
+    def test_func(sc_inner):
+        new_sc = change(sc_inner)
+        return new_sc
+
+    sc = test_func(eng)
+    assert sc.defaultParallelism == target_cores
+    sc.stop()
+
+
+def test_no_sc():
+    @fallback
+    def test_func(arg1):
+        raise ValueError(arg1)
+
+    result = test_func(10)
+    assert result is None
+
+
+def test_stopped_sc(eng):
+    @fallback
+    def test_func(sc_inner):
+        sc_inner.stop()
+        raise ValueError(10)
+
+    result = test_func(eng)
+    assert result is None
