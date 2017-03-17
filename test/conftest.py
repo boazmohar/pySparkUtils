e8213e15d75207ad2567bbae4e204af3db935a59
Mohar, Boaz <moharb@janelia.hhmi.org>
2017-03-17 12:49:10 -0400

    Added test to checking executers.

--- test/conftest.py
+++ test/conftest.py
@@ -13,7 +13,11 @@ def eng():
         parent = os.path.split(dir1)[0]
         spark_home = os.path.split(parent)[0]
         os.environ['SPARK_HOME'] = spark_home
-    sc = SparkContext()
+    # figure out the number of cores to use
+    cores = multiprocessing.cpu_count()
+    if cores <= 2:
+        raise RuntimeWarning('some test would fail with only 2 cores')
+    sc = SparkContext(master='local[' + str(cores-1) + ']')
     yield sc
     sc.stop()
     del sc
