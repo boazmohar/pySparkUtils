9b89d8cb3d89d0d3e3adce03902fef464d65f0bb
Boaz Mohar <boazmohar@gmail.com>
2017-07-27 09:51:34 -0400

    moving to new pypi.org

--- setup.py
+++ setup.py
@@ -1,15 +1,39 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+here = path.abspath(path.dirname(__file__))
+
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    requires = f.read().splitlines()
+
+with open(path.join(here, 'requirements-dev.txt'), encoding='utf-8') as f:
+    requires_dev = f.read().splitlines()
 
 setup(
     name='pySparkUtils',
-    packages=['pySparkUtils'],
+    packages=find_packages(exclude=['dist', 'docs', 'test']),
     version='0.1.6',
     description="A collection of utilities for handling pySpark's SparkContext",
+    long_description=long_description,
     author='Boaz Mohar',
     author_email='boazmohar@gmail.com',
+    license='MIT',
     url='https://github.com/boazmohar/pySparkUtils',
     download_url='https://github.com/boazmohar/pySparkUtils/archive/0.1.6.tar.gz',
     keywords=['spark', 'pyspark', ],
-    classifiers=[],
-    install_requires=open('requirements.txt').read().split('\n'),
+    classifiers=['Development Status :: 3 - Alpha',
+                 'License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.5',
+                 ],
+    install_requires=requires,
+    extras_require={
+        'dev': requires_dev,
+    },
 )
