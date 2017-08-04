35e3757fe3dc52b9c6c0a873bec9bd5bcfe6a830
Boaz Mohar <boazmohar@gmail.com>
2017-08-04 19:25:57 -0400

    More svd tests of normalization type

--- test/test_svd.py
+++ test/test_svd.py
@@ -11,7 +11,9 @@ pytestmark = pytest.mark.usefixtures("eng")
 def test_svd(eng):
     x_local = make_low_rank_matrix(n_samples=10, n_features=50, random_state=0)
     x = fromarray(x_local.reshape(10, 10, 5), engine=eng)
-
+    x.cache()
+    x.count()
+    
     u1, s1, v1 = randomized_svd(x_local, n_components=2,  random_state=0)
 
     u2, v2, s2 = getSVD(x, k=2, getComponents=True, getS=True)
@@ -19,3 +21,25 @@ def test_svd(eng):
     assert s1.shape == s2.shape
     assert v1.shape == (2, 50)
     assert v2.shape == (2, 10, 5)
+
+    u2, v2, s2 = getSVD(x, k=2, getComponents=True, getS=True, normalization='nanmean')
+    assert u1.shape == u2.shape
+    assert s1.shape == s2.shape
+    assert v1.shape == (2, 50)
+    assert v2.shape == (2, 10, 5)
+
+    u2, v2, s2 = getSVD(x, k=2, getComponents=True, getS=True, normalization='zscore')
+    assert u1.shape == u2.shape
+    assert s1.shape == s2.shape
+    assert v1.shape == (2, 50)
+    assert v2.shape == (2, 10, 5)
+
+    u2, v2, s2 = getSVD(x, k=2, getComponents=True, getS=True, normalization=None)
+    assert u1.shape == u2.shape
+    assert s1.shape == s2.shape
+    assert v1.shape == (2, 50)
+    assert v2.shape == (2, 10, 5)
+
+    with pytest.raises(RuntimeError) as ex:
+        u2, v2, s2 = getSVD(x, k=2, getComponents=True, getS=True, normalization=None)
+    assert 'Normalization should be one of' in str(ex.value)
