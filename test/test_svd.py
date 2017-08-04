f29668a1c7688e441ca6ae58b5fadf7fe63feeba
Boaz Mohar <boazmohar@gmail.com>
2017-08-04 09:31:01 -0400

    problem with rev requierments

--- test/test_svd.py
+++ test/test_svd.py
@@ -1,7 +1,10 @@
-from sklearn.datasets import make_low_rank_matrix
 import pytest
+from sklearn.datasets import make_low_rank_matrix
+from sklearn.utils.extmath import randomized_svd
 from thunder.images import fromarray
+
 from pySparkUtils.SVD import getSVD
+
 pytestmark = pytest.mark.usefixtures("eng")
 
 
@@ -9,7 +12,6 @@ def test_svd(eng):
     x_local = make_low_rank_matrix(n_samples=10, n_features=50, random_state=0)
     x = fromarray(x_local.reshape(10, 10, 5), engine=eng)
 
-    from sklearn.utils.extmath import randomized_svd
     u1, s1, v1 = randomized_svd(x_local, n_components=2,  random_state=0)
 
     u2, v2, s2 = getSVD(x, k=2, getComponents=True, getS=True)
