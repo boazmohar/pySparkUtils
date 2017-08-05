94b567e2eb331a460b0879e16f19ce27c0c5a5fc
Boaz Mohar <boazmohar@gmail.com>
2017-08-04 20:10:16 -0400

    wrong error type

--- test/test_svd.py
+++ test/test_svd.py
@@ -40,6 +40,6 @@ def test_svd(eng):
     assert v1.shape == (2, 50)
     assert v2.shape == (2, 10, 5)
 
-    with pytest.raises(RuntimeError) as ex:
+    with pytest.raises(ValueError) as ex:
         u2, v2, s2 = getSVD(x, k=2, getComponents=True, getS=True, normalization='error')
     assert 'Normalization should be one of' in str(ex.value)
